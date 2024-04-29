1b6c5abb2f75b04addc92709d14e15403bf46fbb
belenarriaga <belen02arriaga@gmail.com>
2024-04-28 23:17:03 -0500

    Examen de programacion orientada a objetos

---
+++ ExamenPoo/src/main/java/pe/edu/upeu/examenpoo/modelo/Producto.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pe.edu.upeu.examenpoo.modelo;
+
+import lombok.Data;
+
+/**
+ *
+ * @author macbookpro
+ */
+@Data
+public class Producto {
+    String dniruc;
+    String nombres;
+    String documento;
+}
---
+++ ExamenPoo/src/test/java/pe/edu/upeu/examenpoo/ExamenPooApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upeu.examenpoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenPooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
